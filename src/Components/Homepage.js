168e9bbaaa7d3e94af08615798bedb15b307e034
amcmullen <amcmullen9291@gmail.com>
2022-01-22 17:23:25 -0500

    init commit

---
+++ src/Components/Homepage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Pagination from '@material-ui/lab/Pagination';
+
+export default function Homepage() {
+return (
+	<div>
+        <center><h1>Home Page</h1></center>
+	</div>
+);
+}
