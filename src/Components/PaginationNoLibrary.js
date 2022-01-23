292583ee378cc49b1d2881b894d02b069ae4b05f
amcmullen <amcmullen9291@gmail.com>
2022-01-23 17:38:28 -0500

    safety commit

--- src/Components/PaginationNoLibrary.js
+++ src/Components/PaginationNoLibrary.js
@@ -3,8 +3,9 @@ import React, { useEffect, useState } from "react";
 const renderData = (data) => {
   return (
     <ul>
-      {data.map((todo, index) => {
-        return <li key={index}>{todo.title}</li>;
+      {data.map((person, id) => {
+        return <><li key={id}>{person.name}</li><li>{person.country}</li><hr/></>;
+
       })}
     </ul>
   );
@@ -25,90 +26,23 @@ function PaginationNoLibrary() {
   }
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos")
+    fetch("https://api.instantwebtools.net/v1/airlines")
       .then((response) => response.json())
-      .then((json) => setData(json));
+      .then((resp) => setData(resp))
   }, []);
   
-  const handleClick = (event) => {
-    setcurrentPage(Number(event.target.id));
-  };
-  
-  const renderPageNumbers = pages.map((number) => {
-    if (number < maxPageNumberLimit + 1 && number > minPageNumberLimit) {
-      return (
-        <li
-          key={number}
-          id={number}
-          onClick={handleClick}
-          className={currentPage == number ? "active" : null}
-        >
-          {number}
-        </li>
-      );
-    } else {
-      return null;
-    }
-  });
-  
-    const handleNextbtn = () => {
-    setcurrentPage(currentPage + 1);
-
-    if (currentPage + 1 > maxPageNumberLimit) {
-      setmaxPageNumberLimit(maxPageNumberLimit + pageNumberLimit);
-      setminPageNumberLimit(minPageNumberLimit + pageNumberLimit);
-    }
-  };
-
-  const handlePrevbtn = () => {
-    setcurrentPage(currentPage - 1);
-
-    if ((currentPage - 1) % pageNumberLimit == 0) {
-      setmaxPageNumberLimit(maxPageNumberLimit - pageNumberLimit);
-      setminPageNumberLimit(minPageNumberLimit - pageNumberLimit);
-    }
-  };
-  
-    let pageIncrementBtn = null;
-  if (pages.length > maxPageNumberLimit) {
-    pageIncrementBtn = <li onClick={handleNextbtn}> &hellip; </li>;
+  if(Object.keys(data).length > 1) {
+    console.log("data set: ", data);
   }
-
-  let pageDecrementBtn = null;
-  if (minPageNumberLimit >= 1) {
-    pageDecrementBtn = <li onClick={handlePrevbtn}> &hellip; </li>;
-  }
-  
+    
 
   return (
     <>
-      <h1>Todo List</h1> <br />
+      <h1>Passenger Listings</h1> <br />
       <a href={'/'}>Return Home</a>
-
+      <div>
       {renderData(data)}
-
-      <ul className="pageNumbers">
-        <li>
-          <button
-            onClick={handlePrevbtn}
-            disabled={currentPage == pages[0] ? true : false}
-          >
-            Prev
-          </button>
-        </li>
-        {pageDecrementBtn}
-        {renderPageNumbers}
-        {pageIncrementBtn}
-        <li>
-          <button
-            onClick={handleNextbtn}
-            disabled={currentPage == pages[pages.length - 1] ? true : false}
-          >
-            Next
-          </button>
-        </li>
-      </ul>
-
+      </div>
     </>
   );
 }
