5477c972cfead3ce1ed7f916a864c40a8cdb1d4d
amcmullen <amcmullen9291@gmail.com>
2022-01-24 11:41:39 -0500

    manual page by page navigation

--- src/Components/PaginationNoLibrary.js
+++ src/Components/PaginationNoLibrary.js
@@ -3,15 +3,33 @@ import React, { useEffect, useState } from "react";
 
 function PaginationNoLibrary() {
     const [data, setData] = useState([]);
+    let [page, setPage ] = useState(1);
 
   useEffect(() => {
-    fetch("https://api.instantwebtools.net/v1/passenger?page=0&size=10")
+    fetch(`https://api.instantwebtools.net/v1/passenger?page=${page}&size=10`)
       .then((response) => response.json())
       .then((resp) => setData(resp.data))
-  }, []);
+  }, [page]);
   
   if(Object.keys(data).length > 1) {
     console.log("data set: ", data);
+  }
+
+  function changePageUp(e){
+    e.preventDefault();
+    page+=1;
+    setPage(page);
+  }
+
+  function changePageDown(e){
+    e.preventDefault();
+    page-=1;
+    setPage(page);
+    if(page<0){
+      page = 0;
+      alert("no more entries")
+      setPage(page);
+    }
   }
 
 
@@ -20,6 +38,10 @@ function PaginationNoLibrary() {
     <div id="sidenav">
       <h1>Passenger Listings</h1><br />
       <a href={'/'}>Return Home</a>
+      <br/><br/>
+      <button className="navigationButtons" onClick={(e) => {changePageDown(e)}}>{`<`}Previous Page </button>
+      <button className="navigationButtons" onClick={(e) => {changePageUp(e)}}> Next Page{`>`}</button>
+      <div>Page # {page}</div>
       </div>
       {data && (
       <div id="mainBackgroundNoLibrary"><table id="FetchWithLimitTable">
